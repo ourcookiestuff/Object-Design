1c1dabe9f8627765b5b6c5a1979709bbf794afaf
Kuba <jakubdziurka2002@gmail.com>
2026-04-10 00:26:35 +0200

    4.0 obsługa logowania

--- zad3/src/main/kotlin/com/object_des/auth/controller/AuthController.kt
+++ zad3/src/main/kotlin/com/object_des/auth/controller/AuthController.kt
@@ -1,17 +1,27 @@
 package com.object_des.auth.controller
 
 import com.object_des.auth.model.UserResponse
+import com.object_des.auth.model.LoginRequest
 import com.object_des.auth.service.AuthService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/auth")
-class AuthController(private val eagerAuthService: AuthService) {
+class AuthController( @Qualifier("authService") private val authService: AuthService ) {
     @GetMapping("/user")
     fun getUser() = listOf(
         UserResponse(1, "john_doe", "john.doe@example.com"),
         UserResponse(2, "jane_doe", "jane.doe@example.com"),
     )
+
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequest): String {
+        return if (authService.authenticate(request.username, request.password)) {
+            "Login successful"
+        } else {
+            "Invalid credentials"
+        }
+    }
+
 }
---
+++ zad3/src/main/kotlin/com/object_des/auth/model/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.object_des.auth.model
+
+data class LoginRequest(
+    val username: String,
+    val password: String
+)
