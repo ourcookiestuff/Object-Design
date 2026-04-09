0b705bee278786ed2ee16985037ad05ff54a7917
Kuba <jakubdziurka2002@gmail.com>
2026-04-10 00:38:28 +0200

    4.5 autowired constructor injection

--- zad3/src/main/kotlin/com/object_des/auth/AuthApplication.kt
+++ zad3/src/main/kotlin/com/object_des/auth/AuthApplication.kt
@@ -1,10 +1,21 @@
 package com.object_des.auth
 
+import com.object_des.auth.service.AuthService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import jakarta.annotation.PostConstruct
 
 @SpringBootApplication
-class AuthApplication
+class AuthApplication @Autowired constructor( @Qualifier("authService") private val authService: AuthService ) {
+	@PostConstruct
+	fun init() {
+		println("AuthApplication started - testing AuthService:")
+        println("john_doe/1234: ${authService.authenticate("john_doe", "password123")}")
+        println("john_doe/wrong: ${authService.authenticate("john_doe", "wrong")}")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<AuthApplication>(*args)
